538ea8fe9d29aec28a18d582206ea9918c00a0e8
AleksandrVasilko <avasilko7@gmail.com>
2022-02-19 20:28:50 +0200

    add App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import ContactForm from "./ContactForm/ContactForm";
 import ContactsList from './ContactsList/ContactsList'
 import Filter from './Filter/Filter';
